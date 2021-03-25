340a88114840e74fc85481464f63b02bac6d1716
Saffd <saffd@tut.by>
2021-03-25 20:52:52 +0300

    init project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [Range(1,500)]
+    public int min = 1;
+
+    [Range(501, 1000)]
+    public int max = 1000;
+    private int guess;
+    void Start()
+    {
+        print($"Загадай число от {min} до {max}");
+        UpdateGuess();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Game Over");
+        }
+    }
+    void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print($"Твое число равно {guess}?");
+    }
+}
